be8e453d77d0bcc77a3146e2792e1e289b2a5ce0
GerardRey <g.rey36500@gmail.com>
2021-07-10 10:02:56 +0200

    ascii table commands file

--- handler/index.js
+++ handler/index.js
@@ -1,5 +1,9 @@
 const { glob } = require("glob");
 const { promisify } = require("util");
+const ascii = require('ascii-table')
+let table = new ascii("Commands");
+
+table.setHeading('Command', ' Load status');
 
 const globPromise = promisify(glob);
 
@@ -10,12 +14,17 @@ module.exports = async (client) => {
         const file = require(value);
         const splitted = value.split("/");
         const directory = splitted[splitted.length - 2];
+        const filename = splitted[splitted.length - 1];
 
         if (file.name) {
             const properties = { directory, ...file };
             client.commands.set(file.name, properties);
-        }
+            table.addRow(filename,'✅')
+        } else
+            table.addRow(filename, '❌ -> Missing a help.name, or help.name is not a string.');
     });
+
+    console.log(table.toString());
 
     // Events
     const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
