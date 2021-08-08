b9b4747bc0bf9235428d0eb05445a8ba7b803df3
Lim Xuan <olimxuan2005@gmail.com>
2021-08-08 14:40:30 +0800

    more options for member

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -19,6 +19,7 @@ client.on("interactionCreate", async (interaction) => {
                 });
             } else if (option.value) args.push(option.value);
         }
+        interaction.member = interaction.guild.members.cache.get(interaction.user.id);
 
         cmd.run(client, interaction, args);
     }
