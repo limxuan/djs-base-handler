8f5fc0f4f2ddbaee79da14c173f758b457ab7e5e
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2021-09-24 12:24:49 +0300

    Mention the bot to use as a prefix
    You can mention the bot instead using a prefix like `@bot ping`.
    Also if you just ping the bot it returns the actual prefix for the server (you can configure the bot to have different prefixes on each server either by saving the prefix to the database or caching it in memory)

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,20 +1,32 @@
 const client = require("../index");
 
 client.on("messageCreate", async (message) => {
-    if (
-        message.author.bot ||
-        !message.guild ||
-        !message.content.toLowerCase().startsWith(client.config.prefix)
-    )
-        return;
+  const mentionRegex = RegExp(`^<@!?${client.user.id}>$`);
+  const mentionRegexPrefix = RegExp(`^<@!?${client.user.id}> `);
 
-    const [cmd, ...args] = message.content
-        .slice(client.config.prefix.length)
-        .trim()
-        .split(" ");
+  let prefix = message.content.match(mentionRegexPrefix)
+    ? message.content.match(mentionRegexPrefix)[0]
+    : client.config.prefix;
 
-    const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
+  if (message.content.match(mentionRegex))
+    message.channel.send(
+      `My prefix for ${message.guild.name} is \`${prefix}\`.`
+    );
 
-    if (!command) return;
-    await command.run(client, message, args);
+  if (
+    message.author.bot ||
+    !message.guild ||
+    !message.content.toLowerCase().startsWith(prefix)
+  )
+    return;
+
+  const [cmd, ...args] = message.content.slice(prefix.length).trim().split(" ");
+
+  const command =
+    client.commands.get(cmd.toLowerCase()) ||
+    client.commands.find((c) => c.aliases?.includes(cmd.toLowerCase()));
+
+  if (!command) return;
+  await command.run(client, message, args);
 });
+
