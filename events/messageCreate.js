d230ede69d5359eda578ef29e682d119ea574486
EPD <75964362+Dot-roasting@users.noreply.github.com>
2022-03-21 04:56:12 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -7,14 +7,13 @@ client.on("messageCreate", async (message) => {
         !message.guild ||
         !message.guild.me.permissions.has('SEND_MESSAGES') ||
         !message.guild.me.permissionsIn(message.channel).has('SEND_MESSAGES') ||
-        !message.content.toLowerCase().startsWith(client.config.prefix)
     )
         return;
 
-    const [cmd, ...args] = message.content
-        .slice(client.config.prefix.length)
-        .trim()
-        .split(/ +/g);
+        if (!message.content.toLowerCase().startsWith(client.config.prefix)) return;
+        const args = message.content.slice(client.config.prefix.length.trim().split(/ +/);
+
+        const cmd = args.shift();
 
     const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
     if (!message.member.permissions.has(command.memberPermissions || [])) return message.channel.send({content: `I need **\`${command.memberPermissions}\`** to use this command!`});
