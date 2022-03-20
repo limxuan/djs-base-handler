d39d41e4c805fe8c583e1e9415a4cce5bc95c862
EPD <75964362+Dot-roasting@users.noreply.github.com>
2022-03-21 04:52:31 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,6 +4,8 @@ client.on("messageCreate", async (message) => {
     if (
         message.author.bot ||
         !message.guild ||
+        !message.guild.me.permissions.has('SEND_MESSAGES') ||
+        !message.guild.me.permissionsIn(message.channel).has('SEND_MESSAGES') ||
         !message.content.toLowerCase().startsWith(client.config.prefix)
     )
         return;
@@ -14,7 +16,7 @@ client.on("messageCreate", async (message) => {
         .split(/ +/g);
 
     const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
-
+    if (!message.member.permissions.has(command.memberPermissions || [])) return message.channel.send({content: `I need **\`${command.memberPermissions}\`** to use this command!`});
     if (!command) return;
     await command.run(client, message, args);
 });
